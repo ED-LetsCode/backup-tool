694c5e2d2b8b37e73239a6e60eb3f19fae061d14
= <=>
2023-03-08 15:07:49 +0100

    update

--- app/api/sshAuth/route.ts
+++ app/api/sshAuth/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { SSHLoginData } from "@/app/Components/ConnectionInput";
 
+import * as ssh2 from "ssh2";
+
 export async function POST(request: Request) {
   const sshLoginData: SSHLoginData = await request.json();
   createSSHConnection(sshLoginData);
@@ -9,4 +11,27 @@ export async function POST(request: Request) {
 
 function createSSHConnection(sshLoginData: SSHLoginData) {
   console.log(sshLoginData);
+
+  // const config: ssh2.ConnectConfig = {
+  //   host: sshLoginData.server,
+  //   port: 22,
+  //   username: sshLoginData.username,
+  //   password: sshLoginData.password,
+  // };
+
+  // // Create a new SSH client
+  // const client = new ssh2.Client();
+
+  // // Connect to the server
+  // client.on("ready", () => {
+  //   console.log("Connected to server");
+  //   // Do something with the SSH connection here
+  // });
+
+  // client.connect(config);
+
+  // // Handle errors
+  // client.on("error", (err) => {
+  //   console.error("Error connecting to server:", err);
+  // });
 }
